5f05371f90722e995173c5542e16dcf3b02d2836
Mariia Masliuk <mariia_masliuk@epam.com>
2024-06-26 08:56:23 +0300

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+console.log("testing");
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/123/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Greeting = () => {
+  return (
+    <div>
+      <h2>Hello World!</h2>
+      <p>It's a good day</p>
+    </div>
+  );
+};
+
+export default Greeting;
